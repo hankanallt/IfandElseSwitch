880b72b68013f91aef0349dd39cd0b90baa8af5f
Harkanwal Singh <hankanallt@gmail.com>
2019-11-29 18:43:01 +0100

    First Commit

---
+++ src/ifandelse/IfandElse.java
@@ -0,0 +1,102 @@
+
+package ifandElse;
+
+
+public class IfandElse {
+
+    
+    public static void main(String[] args) {
+        
+     int i = 3;
+     
+     switch (i) {
+        
+        case 1:
+        System.out.println("i is 1");
+        break;
+        
+        case 2:
+        System.out.println("i is 2");
+        break;
+        
+        case 3:
+        System.out.println("i is 3");
+        break;
+        
+        case 4:
+        System.out.println("i is 4");
+        break;
+        
+        case 5:
+        System.out.println("i is 5");
+        break;
+        
+        default:
+        System.out.println("i is 5");
+        break;
+    }
+             
+        
+    }
+    
+}
+        
+//        int i = 0;
+//        int j = 0;
+//        
+//        if(i%2 == 0 && j%2 ==0)
+//            System.out.println("I and J are even");
+//        
+//        if(i%2 == 0 || j%2 ==0)
+//            System.out.println("Either I or J are even");
+//        
+//        if(!(i%2 == 0))
+//            System.out.println("i is not even");
+//        
+//    }
+//}
+//        int i=1;
+//        if(i>0)
+//            System.out.println(i+ "is Positive.");
+//        else
+//            if(i < 0)
+//                System.out.println(i+ "is Negative");
+//        else
+//                System.out.println(i+ "is Zero");
+//        
+//    }
+//}
+        
+//        int i = 5;
+//        if (i%2 ==0)
+//            System.out.println(i + "\nIs an even number.");
+//        else
+//            System.out.println(i + "\nIs an odd number.");
+//    }
+//    
+//}
+
+
+
+
+//Comparison operators
+
+//== Equals to
+//!= Not Equal to
+// > Greater than
+//  >= Greater than or Equal to
+// < Less than
+// <= Less than or Equal to
+
+//Operators
+
+//And &&
+//Or ||
+//Not !
+
+// Switch
+//The switch statement takes a variable and then has a list of cases or actions to
+//perform for each value that the variable obtains.
+
+//It uses the word default to perform actions in which the conditions are not met
+//and it uses the word break to stop performing actions.
